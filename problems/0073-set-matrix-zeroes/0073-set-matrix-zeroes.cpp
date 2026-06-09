4b37990b154b9ad91abf492fa90bfcfd1986123a
Paul978 <soumyadeep.dev.28@gmail.com>
2026-06-09 19:33:41 +0000

    LeetCode Synchronization - Set Matrix Zeroes (C++)

---
+++ problems/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        bool rowZero = false, colZero = false;
+
+    for (int j = 0; j < n; j++) {
+        if (matrix[0][j] == 0) {
+            rowZero = true;
+            break;
+        }
+    }
+    for (int i = 0; i < m; i++) {
+        if (matrix[i][0] == 0) {
+            colZero = true;
+            break;
+        }
+    }
+
+    // Step 3: Traverse the rest (excluding first row & col)
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;  // mark row
+                matrix[0][j] = 0;  // mark col
+            }
+        }
+    }
+
+    // Step 4: Update the rest
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if (rowZero) {
+        for (int j = 0; j < n; j++) matrix[0][j] = 0;
+    }
+    if (colZero) {
+        for (int i = 0; i < m; i++) matrix[i][0] = 0;
+    }
+    }
+};
