f7fdd57c49e3c76d04a079f904632abfa82b5b09
Paul978 <soumyadeep.dev.28@gmail.com>
2026-07-06 09:27:43 +0000

    LeetCode Synchronization - Remove Covered Intervals (C++)

---
+++ problems/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](vector<int> &a,vector<int> &b)
+        {
+            if(a[0]==b[0])
+                return a[1]>b[1];
+            return a[0]<b[0];
+        });
+
+        int ans=0;
+        int mx=0;
+
+        for(auto &v:intervals)
+        {
+            if(v[1]>mx)
+            {
+                ans++;
+                mx=v[1];
+            }
+        }
+
+        return ans;
+    }
+};
