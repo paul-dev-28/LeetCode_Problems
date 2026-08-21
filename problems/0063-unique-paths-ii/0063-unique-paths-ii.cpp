30cfab63785209c0ae271c514d13af0f7ef10a88
Paul978 <soumyadeep.dev.28@gmail.com>
2026-08-21 18:25:23 +0000

    LeetCode Synchronization - Unique Paths II (C++)

---
+++ problems/0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int> dp(n,0);
+        dp[0]=1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                    dp[j]=0;
+                else if(j>0)
+                    dp[j]+=dp[j-1];
+            }
+        }
+        return dp[n-1];
+    }
+};
