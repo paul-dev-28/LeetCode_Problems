a1d79393f71326807eb3796ba268907b30179905
Paul978 <soumyadeep.dev.28@gmail.com>
2026-09-17 11:46:10 +0530

    Time: 7 ms (75.52%) | Memory: 88.9 MB (67.53%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,27 +1,28 @@
-class Solution
-{
+class Solution {
 public:
     int minSumOfLengths(vector<int>& arr,int target)
     {
-        int n=arr.size(),ans=1e9;
-        vector<int> best(n,1e9);
-        unordered_map<long long,int> mp;
-        mp[0]=-1;
-        long long sum=0;
-        int cur=1e9;
-        for(int i=0;i<n;i++)
+        int n=arr.size();
+        vector<int> dp(n,1e9);
+        int ans=1e9;
+        int left=0,sum=0;
+        int best=1e9;
+        for(int right=0;right<n;right++)
         {
-            sum+=arr[i];
-            if(mp.count(sum-target))
+            sum+=arr[right];
+            while(sum>target)
             {
-                int j=mp[sum-target]+1;
-                int len=i-j+1;
-                if(j>0 && best[j-1]!=1e9)
-                    ans=min(ans,best[j-1]+len);
-                cur=min(cur,len);
+                sum-=arr[left];
+                left++;
             }
-            best[i]=cur;
-            mp[sum]=i;
+            if(sum==target)
+            {
+                int len=right-left+1;
+                if(left>0 && dp[left-1]!=1e9)
+                    ans=min(ans,len+dp[left-1]);
+                best=min(best,len);
+            }
+            dp[right]=best;
         }
         return ans==1e9?-1:ans;
     }
